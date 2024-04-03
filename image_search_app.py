b3806bac37fde44cf1812ed890d071fb3daf7a30
alexioana <alexander.ioana@gmail.com>
2024-04-03 11:10:49 +0100

    streamlit form to avoid refreshing on parameter changes

--- image_search_app.py
+++ image_search_app.py
@@ -11,7 +11,7 @@ qd_client = QdrantClient("localhost", port=6333)
 model = SentenceTransformer("clip-ViT-B-32")
 
 
-def search_dataset(text_query: str):
+def search_dataset(text_query: str, results_limit: int, confidence_threshold: float):
     """
     Perform a semantic search based on the text query, in the ad dataset collection.
     :param text_query: string representing a textual query
@@ -21,7 +21,8 @@ def search_dataset(text_query: str):
         collection_name="ad_dataset",
         query_vector=model.encode(text_query).tolist(),
         with_payload=True,
-        score_threshold=0.2
+        limit=results_limit,
+        score_threshold=confidence_threshold
     )
 
     return results
@@ -33,7 +34,7 @@ def display_results(results):
     """
 
     st.header('Results')
-    st.write('Query found {} results  with a score >0.2 in the database...'.format(len(results)))
+    st.write('Query found {} results in the database...'.format(len(results)))
 
     if len(results) == 0:
         return
@@ -49,9 +50,18 @@ def display_results(results):
 st.set_page_config(page_title="Image Search Engine", page_icon="🧐", layout="wide")
 st.title("Image Search Engine")
 
-text_search = st.text_input("Search dataset", value="")
+# form to submit a query
+with st.form("text2image_search"):
+    text_search = st.text_input("Search dataset", value="")
 
-# on submitting a search
-if text_search:
-    query_results = search_dataset(text_search)
-    display_results(query_results)
+    # add sliders for control over score and results limit in one row.
+    col1, col2 = st.columns(2)
+    limit_slider = col1.slider(label='number of results', min_value=4, max_value=50, value=4)
+    confidence_threshold = col2.slider(label='score threshold', min_value=0.0, max_value=1.0, value=0.2)
+
+    # on submitting a search
+    submit_search = st.form_submit_button("🔎")
+
+    if submit_search:
+        query_results = search_dataset(text_search, limit_slider, confidence_threshold)
+        display_results(query_results)
