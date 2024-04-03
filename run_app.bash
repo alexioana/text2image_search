59e61f2c8378096575b39f8fabedcb9939b2cd79
alexioana <alexander.ioana@gmail.com>
2024-04-03 11:14:19 +0100

    run streamlit and qdrant in 1 cmd

--- run_app.bash
+++ run_app.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Open the second terminal window and run a command
+gnome-terminal -- bash -c 'venv/bin/streamlit run image_search_app.py;'
+
 # Open the first terminal window and run a command
 gnome-terminal -- bash -c 'docker run -p 6333:6333 -p 6334:6334 -v $(pwd)/qdrant_storage:/qdrant/storage:z qdrant/qdrant;'
-
-# Open the second terminal window and run a command
-gnome-terminal -- bash -c 'venv/bin/streamlit run image_search_app.py; sleep 5;'
 
 
 
